b4ee13590a94b3ae5e4cc7823f32d61959cdda40
Angelo <angelomilienjoseph@gmail.com>
2021-10-11 18:33:52 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function superbowlWin(data) {
+    let result = data.find(data => data.result === "W")
+    // return !!result ? result.year : undefined
+    debugger
+    if (result) {
+        return result.year
+    }
+    else {
+        return undefined
+    };
+}
